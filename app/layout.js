d303ec0d9acf90420b024052fdd604934288a029
aakhimanshu <himanshu@aakoryxglobal.in>
2026-06-02 19:21:47 +0530

    pixel AnalyticsCode Added

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,66 @@
+
 import './globals.css';
+import Script from 'next/script';
+
+export const metadata = {
+  title: 'CreativeMind',
+  description: 'CreativeMind IT Solutions',
+};
 
 export default function RootLayout({ children }) {
-  return <html lang="en">{children}</html>;
+  return (
+    <html lang="en">
+      <body>
+
+        {children}
+
+        {/* Google Ads / Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17350106214"
+        />
+
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+        >
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17350106214');
+          `}
+        </Script>
+
+        {/* Facebook Meta Pixel */}
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+        >
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;
+            n.push=n;
+            n.loaded=!0;
+            n.version='2.0';
+            n.queue=[];
+            t=b.createElement(e);
+            t.async=!0;
+            t.src=v;
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1476825456779046');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+      </body>
+    </html>
+  );
 }
